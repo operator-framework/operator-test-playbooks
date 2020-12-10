5ac3cbe5b7d18a9b2a28827bad5e0b32d8ccdec7
Martin Vala <mavala@redhat.com>
2020-12-10 10:36:46 +0100

    Test
    Signed-off-by: Martin Vala <mavala@redhat.com>

--- upstream/test/test.sh
+++ upstream/test/test.sh
@@ -243,7 +243,7 @@ function ExecParameters() {
     [[ $1 == orange* ]] && [[ $OP_TEST_PROD -eq 1 ]] && [ "$OP_TEST_STREAM" = "upstream-community-operators" ] && OP_TEST_EXEC_USER="$OP_TEST_EXEC_USER -e quay_api_token=$QUAY_API_TOKEN_OPERATORHUBIO -e bundle_registry=quay.io -e bundle_image_namespace=operatorhubio -e bundle_index_image_namespace=operatorhubio -e bundle_index_image_name=catalog"
 
     # Only for testing use case
-    [[ $1 == orange* ]] && [[ $OP_TEST_PROD -eq 2 ]] && OP_TEST_EXEC_USER="$OP_TEST_EXEC_USER -e quay_api_token=$QUAY_API_TOKEN_OPERATOR_TESTING -e bundle_registry=quay.io -e bundle_image_namespace=operator_testing_community -e bundle_index_image_namespace=operator_testing_community -e bundle_index_image_name=catalog"
+    [[ $1 == orange* ]] && [[ $OP_TEST_PROD -eq 2 ]] && OP_TEST_EXEC_USER="$OP_TEST_EXEC_USER -e quay_api_token=$QUAY_API_TOKEN_OPERATOR_TESTING -e bundle_registry=quay.io -e bundle_image_namespace=operator_testing -e bundle_index_image_namespace=operator_testing -e bundle_index_image_name=catalog"
 
 
     # If community and doing orange_<version>
