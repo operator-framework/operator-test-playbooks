fe82d8141277c9a6bcd45787bc3fb57bfb48da59
Martin Vala <mavala@redhat.com>
2021-03-17 11:42:44 +0100

    TEst
    Signed-off-by: Martin Vala <mavala@redhat.com>

--- upstream/test/oprt_diff.sh
+++ upstream/test/oprt_diff.sh
@@ -7,8 +7,8 @@ OPRT_SRC_BRANCH=${OPRT_SRC_BRANCH-"master"}
 [ -n "$OPRT_REPO" ] || { echo "Error: '\$OPRT_REPO' is empty !!!"; exit 1; }
 [ -n "$OPRT_SHA" ] || { echo "Error: '\$OPRT_SHA' is empty !!!"; exit 1; }
 
-git clone https://github.com/$OPRT_REPO communit-operators
-cd communit-operators
+git clone https://github.com/$OPRT_REPO community-operators
+cd community-operators
 git checkout $(git branch -a --contains $OPRT_SHA | cut -d '/' -f 2-)
 git log --oneline | head
 
